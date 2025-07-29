18dac1ec6411688d83b33215bc2afa61d2f182b6
munyokii <munyoki912@gmail.com>
2025-07-29 23:31:33 +0300

    :sparkles: Personalized Greeting App

---
+++ greeting_app/main.py
@@ -0,0 +1,7 @@
+"""Personalized greeting application."""
+username = input("Enter your name: ")
+color = input("What is your favorite color? ")
+
+print(f"Welcome back, {username}! Your favorite color, {color}, is nice.")
+print("Have a great day ahead!")
+print("Remember, you can always change your preferences later.")
