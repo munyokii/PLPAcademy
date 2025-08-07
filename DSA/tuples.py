048db094d3996066a703eef812f5dc7abd981a12
munyokii <munyoki912@gmail.com>
2025-08-07 23:07:56 +0300

    :sparkles: Tuples in Python

---
+++ DSA/tuples.py
@@ -0,0 +1,23 @@
+var1 = ('hello')
+print(type(var1))
+
+# creating a tuple having one element
+var2 = ("hello",)
+print(type(var2))
+
+# Parentheses is optional
+var3 = 'interesting', 
+print(type(var3))
+
+# Accessing Python Tuple Elements
+letters = ('r', 't', 'y', 'u', 'i', 'o', 'p')
+
+print(letters[0])
+print(letters[4])
+print(letters[-3])
+
+# Python Tuple Methods
+my_tuple = ('s', 'g', 'l', 'k', 'g', 'g')
+
+print(my_tuple.count('g'))
+print(my_tuple.index('g'))
